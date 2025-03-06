f6cfd909659846c9baf5bdadf03f3eb93159777b
Sandesh <msandeah9878@gmail.com>
2025-03-06 21:47:41 +0545

    changes

--- resources/views/Client/Home/index.blade.php
+++ resources/views/Client/Home/index.blade.php
@@ -24,5 +24,4 @@
     // Set SEO variables
     $title = 'Trail Nepal Treks';
     $description = 'Discover your gateway to the Himalayas with seamless Nepal adventure planning. From trekking and expert guides to transport and tours, customize and book your unforgettable journey today!';
-
 @endphp
