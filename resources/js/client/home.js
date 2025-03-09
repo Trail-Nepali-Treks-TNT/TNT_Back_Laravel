7e9c979a49db021161426ab0fd44bbd4abb0bd8b
Puskar Adhikari <puskar.ad@gmail.com>
2025-03-09 15:38:14 +0545

    feat: Add Swiper implementation for early card swiper with responsive breakpoints and navigation

---
+++ resources/js/client/home.js
@@ -0,0 +1,47 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+
+document.addEventListener('DOMContentLoaded', () => {
+    //Card list items
+    new Swiper('.earlyCardSwiper', {
+        modules: [Navigation],
+        spaceBetween: 16,
+        centeredSlides: false,
+        breakpoints: {
+            320: {
+                slidesPerView: 1 // Mobile view
+            },
+            640: {
+                slidesPerView: 2 // Small tablets
+            },
+            768: {
+                slidesPerView: 3 // Tablets
+            },
+            1024: {
+                slidesPerView: 4 // Desktops
+            },
+            1280: {
+                slidesPerView: 4 // Desktops
+            },
+            1440: {
+                slidesPerView: 4 // Desktops
+            },
+            1920: {
+                slidesPerView: 5 // Desktops
+            },
+            2560: {
+                slidesPerView: 6
+            }
+        },
+        allowTouchMove: true,
+        // Optional parameters
+        loop: false, // Enables looping
+
+        navigation: {
+            nextEl: '.tnt-package-slider-next-btn',
+            prevEl: '.tnt-package-slider-prev-btn'
+        }
+    });
+});
