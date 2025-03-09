24de3fb76dc94a1947df47bd962ef34a8a265d0b
Puskar Adhikari <puskar.ad@gmail.com>
2025-03-09 15:37:18 +0545

    refactor: Clean up package card component markup and improve image link structure

--- resources/views/components/package-card.blade.php
+++ resources/views/components/package-card.blade.php
@@ -1,20 +1,19 @@
-
 @props([
-    'images' => [],
-    'title',
-    'description',
-    'expiryDate',
-    'price',
-    'discountedPrice' => null,
-    'discountPercentage' => null,
-    'bestSeller' => false,
-    'popular' => false,
-    'link' => ''
+'images' => [],
+'title',
+'description',
+'expiryDate',
+'price',
+'discountedPrice' => null,
+'discountPercentage' => null,
+'bestSeller' => false,
+'popular' => false,
+'link' => ''
 ])
 @php
-    $currentDate = \Carbon\Carbon::now();
-    $expiry = \Carbon\Carbon::parse($expiryDate);
-    $duration = $currentDate->diffInDays($expiry);
+$currentDate = \Carbon\Carbon::now();
+$expiry = \Carbon\Carbon::parse($expiryDate);
+$duration = $currentDate->diffInDays($expiry);
 @endphp
 
 @vite('resources/css/client-styles/package-card.css')
@@ -22,36 +21,37 @@
     <div class="package-card-img-container position-relative">
         <div class="package-card-badge-container">
             @if($bestSeller)
-                <div class="package-card-badge best-seller">Best Seller</div>
+            <div class="package-card-badge best-seller">Best Seller</div>
             @endif
             @if($popular)
-                <div class="package-card-badge popular">Popular</div>
+            <div class="package-card-badge popular">Popular</div>
             @endif
         </div>
 
         <div class="package-card-swiper-container swiper">
             <div class="swiper-wrapper position-relative">
                 @foreach($images as $image)
-                        <img src="{{ $image }}" class="cover swiper-slide" alt="Package image">
+                <a href="{{ $link}}" class="w-100 swiper-slide">
+                    <img src="{{ $image }}" class="cover " alt="Package image">
+                </a>
                 @endforeach
             </div>
-             <button class="tnt-slider-btn tnt-card-img-slider-next-btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="lucide lucide-arrow-right">
-                                <path d="M5 12h14"></path>
-                                <path d="m12 5 7 7-7 7"></path>
-                            </svg>
-                        </button>
-
-                        <button class="tnt-slider-btn tnt-card-img-slider-prev-btn">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="lucide lucide-arrow-left">
-                                <path d="m12 19-7-7 7-7"></path>
-                                <path d="M19 12H5"></path>
-                            </svg>
-                        </button>
+            <button class="tnt-slider-btn tnt-card-img-slider-next-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" class="lucide lucide-arrow-right">
+                    <path d="M5 12h14"></path>
+                    <path d="m12 5 7 7-7 7"></path>
+                </svg>
+            </button>
+            <button class="tnt-slider-btn tnt-card-img-slider-prev-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                    stroke-linejoin="round" class="lucide lucide-arrow-left">
+                    <path d="m12 19-7-7 7-7"></path>
+                    <path d="M19 12H5"></path>
+                </svg>
+            </button>
         </div>
     </div>
     <div class="package-card-detail">
@@ -74,20 +74,18 @@
             ${{ number_format($price, 2) }}
 
             @if($discountedPrice && $price > 0)
-                            @php
-    $calculatedDiscount = round((($price - $discountedPrice) / $price) * 100);
-                            @endphp
-                                <span class="discount-percentage">Save {{ $calculatedDiscount }}%</span>
+            @php
+            $calculatedDiscount = round((($price - $discountedPrice) / $price) * 100);
+            @endphp
+            <span class="discount-percentage">Save {{ $calculatedDiscount }}%</span>
             @endif
         </div>
         @if($discountedPrice)
-            <div class="current-price d-flex align-items-center gap-1">
-                From ${{ number_format($discountedPrice, 2) }}
-                <span>/person</span>
-            </div>
+        <div class="current-price d-flex align-items-center gap-1">
+            From ${{ number_format($discountedPrice, 2) }}
+            <span>/person</span>
+        </div>
         @endif
     </div>
 </div>
 @vite('resources/js/client/package-card.js')
-
-
