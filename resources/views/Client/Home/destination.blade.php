53e010b93dd00ee3332e4cd9b23709d3b2153e6a
Puskar Adhikari <puskar.ad@gmail.com>
2025-03-11 00:43:32 +0545

    feat: Add destination collection section with multiple regions and tours

--- resources/views/Client/Home/destination.blade.php
+++ resources/views/Client/Home/destination.blade.php
@@ -15,4 +15,70 @@
             Let us handle the planning! Choose from our expertly designed programs for a stress-free, perfectly tailored journey.
         </p>
     </div>
+    <div class="destination-collection">
+        <div>
+            <div class="destination-flex-container">
+                <div class="destination-column">
+                    <a href="#" class="destination-card">
+                        <img alt="Everest Region" src="https://images.unsplash.com/photo-1571401847270-78ab5f316800?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFyZGl8ZW58MHx8MHx8fDA%3D">
+                        <div class="destination-overlay"></div>
+                        <div class="destination-text-container">
+                            <h2>Everest Region</h2>
+                            <p>9 tours</p>
+                        </div>
+                    </a>
+                    <a href="#" class="destination-card">
+                        <img alt="Langtang Region" src="https://images.unsplash.com/photo-1538321833141-b175d44fd264?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QW5uYXB1cm5hJTIwQ2lyY3VpdHxlbnwwfHwwfHx8MA%3D%3D">
+                        <div class="destination-overlay"></div>
+                        <div class="destination-text-container">
+                            <h2>Langtang Region</h2>
+                            <p>9 tours</p>
+                        </div>
+                    </a>
+                </div>
+                <a href="#" class="destination-half-width">
+                    <div class="destination-card">
+                        <img alt="Annapurna Region" src="https://images.unsplash.com/photo-1571401847270-78ab5f316800?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFyZGl8ZW58MHx8MHx8fDA%3D">
+                        <div class="destination-overlay"></div>
+                        <div class="destination-text-container">
+                            <h2>Annapurna Region</h2>
+                            <p>9 tours</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div>
+            <div class="destination-flex-container-reverse">
+                <div class="destination-column">
+                    <a href="#" class="destination-card">
+                        <img alt="Mustang Region (Upper and Lower Mustang)" src="https://images.unsplash.com/photo-1538321833141-b175d44fd264?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QW5uYXB1cm5hJTIwQ2lyY3VpdHxlbnwwfHwwfHx8MA%3D%3D">
+                        <div class="destination-overlay"></div>
+                        <div class="destination-text-container">
+                            <h2>Mustang Region (Upper and Lower Mustang)</h2>
+                            <p>9 tours</p>
+                        </div>
+                    </a>
+                    <a href="#" class="destination-card">
+                        <img alt="Manaslu Region" src="https://images.unsplash.com/photo-1678501218653-d3b60b2e4136?w=500&amp;auto=format&amp;fit=crop&amp;q=60&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGFubmFwdXJuYSUyMGJhc2UlMjBjYW1wfGVufDB8fDB8fHww&quot;">
+                        <div class="destination-overlay"></div>
+                        <div class="destination-text-container">
+                            <h2>Manaslu Region</h2>
+                            <p>9 tours</p>
+                        </div>
+                    </a>
+                </div>
+                <a href="#" class="destination-half-width">
+                    <div class="destination-card">
+                        <img alt="Kanchenjunga Region" src="https://images.unsplash.com/photo-1600786428316-0d6cc71d5181?q=80&amp;w=1964&amp;auto=format&amp;fit=crop&amp;ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+                        <div class="destination-overlay"></div>
+                        <div class="destination-text-container">
+                            <h2>Kanchenjunga Region</h2>
+                            <p>9 tours</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
 </section>
