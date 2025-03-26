05befcd2f009baf4aaae849d3f99719c316cd191
Puskar Adhikari <puskar.ad@gmail.com>
2025-03-26 12:40:44 +0545

    style: Update home page view to use isHomePage variable for conditional rendering

--- resources/views/Client/Home/index.blade.php
+++ resources/views/Client/Home/index.blade.php
@@ -89,5 +89,5 @@ $title = 'Trail Nepal Treks';
 $description = 'Discover your gateway to the Himalayas with seamless Nepal adventure planning. From trekking and expert guides to transport and tours, customize and book your unforgettable journey today!';
 
 //Hero section
-$heroContainerClassName = "home-hero-container"
+$isHomePage = true
 @endphp
