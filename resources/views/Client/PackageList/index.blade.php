eb48323018f4fda6b556a41552567c1e29327eb1
Puskar Adhikari <puskar.ad@gmail.com>
2025-04-19 23:01:39 +0545

    feat: enhance package list page layout and content

--- resources/views/Client/PackageList/index.blade.php
+++ resources/views/Client/PackageList/index.blade.php
@@ -1,6 +1,24 @@
 @extends('Client.layouts.app')
 @section('content')
-    <h1>Package list</h1>
+    <link rel="stylesheet" href="{{ asset('assets/css/client-styles/package-list.css') }}">
+    <div class="package-list-page tnt-container">
+        <x-breadcrumbs :items="[
+            ['name' => 'Home', 'path' => '/'],
+            ['name' => 'Package List Name'],
+        ]" />
+        <h1 class="package-list-title font-playfair text-2xl sm:text-3xl md:text-5xl lg:text-7xl">Annapurna Region</h1>
+        <p class="package-list-desc">The Annapurna region in Nepal offers stunning landscapes, from lush forests to
+            towering
+            peaks
+            like Annapurna I (8,091m). With iconic treks such as the Annapurna Circuit and Base Camp, it
+            combines breathtaking views with rich cultural experiences, making it a top destination for
+            adventurers.</p>
+        <!-- TODO: Update alt attribute for the image -->
+        <img src="https://images.unsplash.com/photo-1620697488876-90f10485e692?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Annapurna Region" class="package-list-cover" />
+
+        <!-- Package lists -->
+    </div>
 @endsection
 @php
     // Set SEO variables
@@ -8,4 +26,5 @@
     $title = 'Package List - Trail Nepal Treks';
     $description = 'Explore our diverse trekking packages in Nepal, including Everest Base Camp, Annapurna Circuit, and Langtang Valley. Join us for an unforgettable adventure!';
     $keywords = 'Trekking packages, Nepal trekking, Everest Base Camp, Annapurna Circuit, Langtang Valley, trekking tours, adventure travel, Trail Nepal Treks';
+
 @endphp
