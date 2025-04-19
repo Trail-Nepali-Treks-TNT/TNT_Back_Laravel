d763173c05a978166918c85f9719360704e185f5
Puskar Adhikari <puskar.ad@gmail.com>
2025-04-19 22:29:32 +0545

    feat:package list page setup

---
+++ resources/views/Client/PackageList/index.blade.php
@@ -0,0 +1,11 @@
+@extends('Client.layouts.app')
+@section('content')
+    <h1>Package list</h1>
+@endsection
+@php
+    // Set SEO variables
+    //TODO: Add SEO variables for the package list page based on package that is being displayed
+    $title = 'Package List - Trail Nepal Treks';
+    $description = 'Explore our diverse trekking packages in Nepal, including Everest Base Camp, Annapurna Circuit, and Langtang Valley. Join us for an unforgettable adventure!';
+    $keywords = 'Trekking packages, Nepal trekking, Everest Base Camp, Annapurna Circuit, Langtang Valley, trekking tours, adventure travel, Trail Nepal Treks';
+@endphp
