7d5330558fb1df864b8858fd1de3178370650d6d
kshetrastha <kshetrastha@gmail.com>
2025-04-20 17:11:05 +0545

    refactor: skeleton for image

--- resources/views/Client/PackageList/index.blade.php
+++ resources/views/Client/PackageList/index.blade.php
@@ -15,7 +15,7 @@
             adventurers.</p>
         <!-- TODO: Update alt attribute for the image -->
         <img src="https://images.unsplash.com/photo-1620697488876-90f10485e692?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="Annapurna Region" class="package-list-cover" />
+            alt="Annapurna Region" class="package-list-cover skeleton" />
 
         <!-- Package lists -->
         <section class="package-list">
